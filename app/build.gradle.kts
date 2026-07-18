3b097af72d2ed10992016d3a3d56eea755623252
Fury <sajsandhu10@gmail.com>
2026-07-18 16:27:07 -0400

    -- fixed project, files were missing after -- it launches on my phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,15 +6,13 @@ plugins {
 android {
     namespace = "com.enigmaticdevs.wallhaven"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.enigmaticdevs.wallhaven"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,3 +24,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "wallhaven"
 include(":app")
+ 
