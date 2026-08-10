abcca9b7bc31d5682773a26b08c946797de962d8
Fury <sajsandhu10@gmail.com>
2026-08-10 15:26:40 -0400

    -- fixed gitignore -- updated gradle --

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.googleFonts)
+    implementation(libs.material3)
     implementation(libs.materialIcons)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
